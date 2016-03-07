7a9ada6c9922db7d41c8fa375b8aa9fda1c47e48
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-03-07 18:31:12 +0200

    Rendering chosen APIs sort of works

--- src/index.tsx
+++ src/index.tsx
@@ -6,29 +6,55 @@ import DevTools from 'mobx-react-devtools';
 import 'isomorphic-fetch';
 import {get_data} from './lib/piper';
 
-class Gatherings {
+const API_BASE = "http://localhost:8000";
+const API_LIST_URL = API_BASE + "/apis";
 
-    API_URL = "http://localhost:8000/apis";
+class ApiView {
+
+    api = null;
+    @observable items = [];
+
+    constructor(api) {
+        this.api = api;
+        get_data(API_BASE + this.api.url, data => this.items = data);
+    }
+
+}
+
+
+class Gatherings {
 
     @observable choices = "";
     @observable apis = [];
+    @observable chosen = [];
 
     makeChange(val) {
         this.choices = val;
     }
 
+    choose(choice) {
+        console.log("choicing", choice);
+        this.chosen.push(new ApiView(choice));
+    }
+
     constructor() {
-        get_data(this.API_URL, data => this.apis = data);
+        get_data(API_LIST_URL, data => this.apis = data);
     }
 }
+
 
 const YourData = ({choices}) => <div>Your data: {choices}</div>;
 
 
-const Apis = ({apis}) => <div>
+const Apis = ({apis, choose}) => <div>
     <ul>
-        {apis.map(item => <li key={item.id}>{item.title}</li>)}
+        {apis.map(item => <li key={item.id} onClick={() => choose(item)}>{item.title}</li>)}
     </ul>
+</div>;
+
+
+const Items = ({api}) => <div key={"item_" + api.api.id}>
+    <span>{api.api.id}</span>
 </div>;
 
 
@@ -39,13 +65,18 @@ class Gather extends React.Component<{gatherings: Gatherings}, {}> {
         gatherings.makeChange(e.target.value);
     }
 
+    makeChoice(choice) {
+        gatherings.choose(choice);
+    }
+
     render() {
-        var {choices, apis} = gatherings;
+        var {choices, apis, choose, chosen} = gatherings;
         return (
             <div>
                 <input value={choices} placeholder="your data" onChange={this.change} />
                 <YourData choices={choices} />
-                {apis? <Apis apis={apis} /> : null}
+                {apis? <Apis apis={apis} choose={this.makeChoice} /> : null}
+                {chosen.map((api,i) => <div key={"items_" + i}><Items api={api} /></div>)}
                 <DevTools />
             </div>
         );
